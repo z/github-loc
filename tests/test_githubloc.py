29f5bbcae26c19f1d9ecd27d4988cd86167fcb89
z <z@xnz.me>
2017-09-09 10:04:45 -0400

    fix the test, duh

--- tests/test_githubloc.py
+++ tests/test_githubloc.py
@@ -5,7 +5,8 @@ import time
 def test_date_parse():
     with open("tests/data/periods.json") as f:
         unique_periods = list(set(json.loads(f.read())))
+        unique_periods.sort()
     time_start = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(unique_periods[0]))
     time_end = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(unique_periods[-1]))
-    assert time_start == "2013-10-05 20:00:00"
-    assert time_end == "2014-09-27 20:00:00"
+    assert time_start == "2009-01-24 19:00:00"
+    assert time_end == "2017-09-02 20:00:00"
